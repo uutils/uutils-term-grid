6a19da716a06bb8da6c172dc6e5329f4588f5101
Terts Diepraam <terts.diepraam@gmail.com>
2023-08-12 18:49:40 +0200

    add license info to big.rs

--- examples/big.rs
+++ examples/big.rs
@@ -1,3 +1,6 @@
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+
 extern crate term_grid;
 use term_grid::{Alignment, Cell, Direction, Filling, Grid, GridOptions};
 
